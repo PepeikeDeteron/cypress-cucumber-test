585034b4eb134618649f05341c2c98382c87e4c7
Yoshiaki Sasaki <pepeikedeteron@gmail.com>
2023-03-28 21:05:06 +0900

    feat: シナリオテストのサンプルを追加

---
+++ cypress/e2e/google.feature
@@ -0,0 +1,10 @@
+Feature: Access Google
+  Google 検索のテスト
+
+  Scenario: Googleで検索する
+    Given google.com にアクセスする
+    Then タイトルに Google がある
+    When "Cypress"で検索する
+    Then 最初の検索結果を取得する
+    And 検索結果が表示される
+    But タイトルに "Unknown" が含まれない
---
+++ cypress/e2e/google.ts
@@ -0,0 +1,37 @@
+import { Given, When, Then, And, But, Before, After } from "@badeball/cypress-cucumber-preprocessor";
+
+Before(() => {
+  cy.clearCookies();
+  cy.clearLocalStorage();
+});
+
+Given("google.com にアクセスする", () => {
+  cy.visit("https://www.google.com");
+});
+
+Then("タイトルに Google がある", () => {
+  cy.title().should("include", "Google");
+});
+
+When("{string}で検索する", (searchTerm: string) => {
+  cy.get("input[name='q']").type(`${searchTerm}{enter}`);
+});
+
+Then("最初の検索結果を取得する", () => {
+  cy.get("#search a")
+  .invoke("attr", "href")
+  .then((href) => console.log(href));
+});
+
+Then("検索結果が表示される", () => {
+  cy.get(".g").should("be.visible");
+});
+
+Then("タイトルに {string} が含まれない", (unwantedTerm: string) => {
+  cy.title().should("not.include", unwantedTerm);
+});
+
+After(() => {
+  cy.clearCookies();
+  cy.clearLocalStorage();
+});
